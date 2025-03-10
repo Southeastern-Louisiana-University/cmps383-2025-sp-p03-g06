4d4f97469c07907a9665e9c7733f751864d83f1e
taylfrad <taylor.fradella@selu.edu>
2025-03-10 16:04:51 -0500

    revert

---
+++ Selu383.SP25.P03.Api/Migrations/20250310210302_Test.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Selu383.SP25.P03.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Theaters_AspNetUsers_ManagerId",
+                table: "Theaters");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ManagerId",
+                table: "Theaters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Theaters_AspNetUsers_ManagerId",
+                table: "Theaters",
+                column: "ManagerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Theaters_AspNetUsers_ManagerId",
+                table: "Theaters");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ManagerId",
+                table: "Theaters",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Theaters_AspNetUsers_ManagerId",
+                table: "Theaters",
+                column: "ManagerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
