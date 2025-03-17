dddb054c9dd639ddc915b75990b5d8b6c94069de
Gdotpierre <98052272+Gdotpierre@users.noreply.github.com>
2025-03-17 18:14:55 -0500

    allowed host in vite config

--- Selu383.SP25.P03.Web/vite.config.ts
+++ Selu383.SP25.P03.Web/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts:["up-eagle-indirectly.ngrok-free.app"],
     proxy: {
       // Proxy API requests to the .NET backend during development
       '/api': {
