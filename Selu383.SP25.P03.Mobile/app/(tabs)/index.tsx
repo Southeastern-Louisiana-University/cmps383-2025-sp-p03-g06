aa734954d50042e6c8294fe7c28f946d577d5ef2
Gdotpierre <98052272+Gdotpierre@users.noreply.github.com>
2025-03-29 20:52:08 -0500

    Mobile Home page

--- Selu383.SP25.P03.Mobile/app/(tabs)/index.tsx
+++ Selu383.SP25.P03.Mobile/app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Lion's Den Cinema</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
